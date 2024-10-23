964b67de3613291dd95e9b1378860af5be7d6b15
DHI\jlaz <l.jasmiina@gmail.com>
2024-10-23 22:50:28 +0200

    Add equipment model test

---
+++ EquipmentStateManagement/Models/Equipment.cs
@@ -0,0 +1,9 @@
+namespace EquipmentStateManagement.Models
+{
+    public class Equipment
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+    }
+}
